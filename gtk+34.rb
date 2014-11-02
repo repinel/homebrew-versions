bce967d7082e7d3538d341ed54ef390ac9f8f710
Roque Pinel <repinel@gmail.com>
2014-11-02 18:03:18 -0500

    gtk+34 3.4.4

---
+++ gtk+34.rb
@@ -0,0 +1,47 @@
+require 'formula'
+
+class Gtkx34 < Formula
+  homepage 'http://gtk.org/'
+  url 'http://ftp.gnome.org/pub/gnome/sources/gtk+/3.4/gtk+-3.4.4.tar.xz'
+  sha256 'f154e460075034da4c0ce89c320025dcd459da2a1fdf32d92a09522eaca242c7'
+
+  depends_on :x11 => ['2.5', :recommended] # needs XInput2, introduced in libXi 1.3
+  depends_on 'pkg-config' => :build
+  depends_on 'glib'
+  depends_on 'jpeg'
+  depends_on 'libtiff'
+  depends_on 'gdk-pixbuf'
+  depends_on 'pango'
+  depends_on 'cairo'
+  depends_on 'jasper' => :optional
+  depends_on 'atk'
+  depends_on 'at-spi2-atk'
+  depends_on 'gobject-introspection'
+  depends_on 'gsettings-desktop-schemas' => :recommended
+
+  def install
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --disable-glibtest
+      --enable-introspection=yes
+      --disable-schemas-compile
+    ]
+
+    if build.without? "x11"
+      args << "--enable-quartz-backend" << "--enable-quartz-relocation"
+    else
+      args << "--enable-x11-backend"
+    end
+
+    system "./configure", *args
+    system "make install"
+    # Prevent a conflict between this and Gtk+2
+    mv bin/'gtk-update-icon-cache', bin/'gtk3-update-icon-cache'
+  end
+
+  def post_install
+    system "#{Formula["glib"].opt_bin}/glib-compile-schemas", "#{HOMEBREW_PREFIX}/share/glib-2.0/schemas"
+  end
+end
